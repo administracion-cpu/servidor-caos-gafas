27a343b1bdfdf50a18513eb7ba372846e3a7d082
administracion-cpu <administracion@bfontibiza.com>
2026-05-17 00:32:36 +0200

    Fix import statement for AppServer
    Fix import statement for AppServer in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Servidor puente CAO-S <-> Gafas Mentra
 // Recibe fotos desde las gafas Mentra y las reenvía al diario de obra del usuario en CAO-S.
-
+// Arreglar import appServer
 import { AppServer } from "@mentra/sdk";
 import fetch from "node-fetch";
 
